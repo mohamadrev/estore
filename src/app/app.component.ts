4aa7e1e511b0bbf869f2fcc3c12a66cb8a8c3029
jamalmohamad <mohammed.jamalhadi@gmail.com>
2020-12-05 18:43:52 +0100

    fix: linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  
-  
   <h1> Szia E-Store <h1>
-<hr>
+  <hr>
   <h5> This is Angular 10-0-8 project <h5>
   <hr>
   <h6> this is a DevOps project conected with Travisci and docker-compose <h6>
   <h6> checking travis 2<h6>
-  
+
   `,
-  //templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.scss']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'estore';
